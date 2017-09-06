cc463c37c8690ae46ebfe11a986f36d2d428b723
eternity-phoenix <769518953@qq.com>
2017-09-06 16:50:32 +0800

    Initial commit

---
+++ src/main/java/com/gzkjg/GzkjgApplication.java
@@ -0,0 +1,13 @@
+package com.gzkjg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class GzkjgApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GzkjgApplication.class, args);
+	}
+}
